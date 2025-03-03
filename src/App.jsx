45cdc7edfe831096c7781afe0d964586ae157ace
kayaththiri-kanagasabai <kkayaththiri@std.foc.sab.ac.lk>
2025-03-03 20:09:05 +0530

    commit all files to git page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Programs from './components/Programs/Programs'
+import Title from './components/Title/Title'
+import About from './components/About/About'
+import Campus from './components/Campus/Campus'
+import Testimonials from './components/Testimonials/Testimonials'
 
 
 const App = () => {
   return (
     <div>
-   <Navbar/>
-   <Hero/>
-  
-  
-  
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Title subtitle="Our PROGRAM" title="What We Ofter" />
+
+        <Programs />
+        <About />
+        <Title subtitle="Gallery" title="Campus Photos" />
+        <Campus />
+        <Title subtitle="TESITIMONIALS" title="what student says" />
+        <Testimonials/>
+     
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './navbar.css'
 import logo from '../../assets/logo.png'
-const navbar = () => {
-  return (
-    <nav className="container">
+const Navbar = () => {
+ 
+ const[sticky,setSticky]=useState(false);
+
+
+ useEffect(()=>{
+   window.addEventListener('scroll',()=>{
+    window.scrollY > 50 ? setSticky(true) : setSticky(false);
+
+   })
+ },[])
+
+ return(
+      
+    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
       <img src={logo} alt="logo" className="logo" />
       <ul>
         <li> Home</li>
         <li> Program</li>
         <li> About us</li>
         <li> Cambus</li>
-        <li> Testimonals</li>
+        <li> Testimonials</li>
         <li>
           <button className="btn">Contact us</button>{" "}
         </li>
@@ -19,4 +31,4 @@ const navbar = () => {
   );
 };
 
-export default navbar
+export default Navbar
