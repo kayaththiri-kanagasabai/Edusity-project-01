c3b6f6101ffa919ff0bf9110e432f8a5d7fc8c43
kayaththiri-kanagasabai <kkayaththiri@std.foc.sab.ac.lk>
2025-03-06 19:38:11 +0530

    commit all files

--- src/components/Videoplayer/Videoplayer.jsx
+++ src/components/Videoplayer/Videoplayer.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Videoplayer.css'
 import video from '../../assets/videoclip.mp4'
 
 const Videoplayer = ({playState,setPlayState}) => {
-  return (
-    <div className={`video-player ${playState?'':'hide'}`}>
+   const player=useRef(null);
+  
+  const closePlayer=(e)=>{
+    if(e.target=== player.current){
+        setPlayState(false)
+    }
+  }
+  
+    return (
+    <div className={`video-player ${playState?'':'hide'}`} ref={player} onClick={closePlayer}>
       <video src={video} autoPlay muted controls></video>
     </div>
   );
